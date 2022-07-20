e1322d8def8f137345d7e1c0e4dbb9976e40ebc0
leonardoferreiramiranda <leonardoferreiramiranda@outlook.com>
2022-07-20 18:48:11 -0300

    meu primeiro commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const titulo = document.querySelector("h1")
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split("")
+    elemento.innerHTML = ""
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i)
+    })
+}
+
+typeWriter(titulo)
